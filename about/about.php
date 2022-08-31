db4b956248ca016fad75510bca7a139668b23ba5
Daniel Di Giovanni <dannyjdigio@gmail.com>
2022-08-31 00:34:06 -0400

    Added about me paragraph.

--- about/about.php
+++ about/about.php
@@ -1,1 +1,9 @@
 <h1>About Me</h1>
+
+<p>
+    I am Daniel Di Giovanni, and I am a 3rd year software engineering student at York University.
+    I've always been fascinated by computers and engineering, especially how complex technical problems can be solved so elegantly.
+    This curiosity motivates me not only in school, but also in studying on my own time.
+    I often research aspects of computers and programming and apply this knowledge in projects I complete on my own.
+    I hope to continue to hone my skills through school and my own endeavors to gain experience as a backend developer, where I can explore new technologies and contribute to solutions to complex problems.
+</p>
