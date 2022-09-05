79401687ef72d6432fd707c1c515f4f56a4547aa
Daniel Di Giovanni <dannyjdigio@gmail.com>
2022-09-05 15:11:05 -0400

    Added content for the projects gallery page.

--- projects/projects.php
+++ projects/projects.php
@@ -1,1 +1,24 @@
-<h1>Projects Overview</h1>
+<h1 class="page-title">My Projects</h1>
+<div id="project-gallery">
+    <div class="project-card">
+        <img class="project-card-image" src="<?= $request_info['base_url_directory'] ?>/static/images/inspirational_website_preview.gif">
+        <div class="project-card-text">
+            <h2 class="project-card-title">Inspirational Website 2.0</h2>
+            <p class="project-card-description">An inspiration website made with Node.js and React.</p>
+        </div>
+    </div>
+    <div class="project-card">
+        <img class="project-card-image" src="<?= $request_info['base_url_directory'] ?>/static/images/tab2xml_preview.gif">
+        <div class="project-card-text">
+            <h2 class="project-card-title">TAB2ML</h2>
+            <p class="project-card-description">Converts text-based music tablature to playable sheet music with Java.</p>
+        </div>
+    </div>
+    <div class="project-card">
+        <img class="project-card-image" src="<?= $request_info['base_url_directory'] ?>/static/images/selection_sort_visualization.gif">
+        <div class="project-card-text">
+            <h2 class="project-card-title">Sorting Algorithm Visualizer</h2>
+            <p class="project-card-description">Visualizes sorting algorithms in real time.</p>
+        </div>
+    </div>
+</div>
