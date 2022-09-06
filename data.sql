bdef3738fb4052cc65dcef133c684accb7dca630
Daniel Di Giovanni <dannyjdigio@gmail.com>
2022-09-05 21:58:12 -0400

    Added an SQL file for the structure of the database (right now only has a table for projects) and an SQL file for loading the database with data (right now only inserts three projects into the projects table).

---
+++ data.sql
@@ -0,0 +1,17 @@
+INSERT INTO projects (
+    'Inspirational Website 2.0',
+    'An inspiration website made with Node.js and React.',
+    'inspirational_website_preview.gif'
+);
+
+INSERT INTO projects (
+    'TAB2ML',
+    'Converts text-based music tablature to playable sheet music with Java.',
+    'tab2xml_preview.gif'
+);
+
+INSERT INTO projects (
+    'Sorting Algorithm Visualizer',
+    'Visualizes sorting algorithms in real time.',
+    'selection_sort_visualization.gif'
+);
---
+++ schema.sql
@@ -0,0 +1,7 @@
+DROP TABLE IF EXISTS projects;
+
+CREATE TABLE projects (
+    title TEXT,
+    description TEXT,
+    display_image TEXT
+);
