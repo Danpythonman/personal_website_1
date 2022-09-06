179c1d1bc9204f1ae5367ceda53e387176e47a1e
Daniel Di Giovanni <dannyjdigio@gmail.com>
2022-09-05 21:59:45 -0400

    Updated the projects overview page to use the database instead of hard-coding information about the projects.

--- projects/projects.php
+++ projects/projects.php
@@ -1,24 +1,27 @@
 <h1 class="page-title">My Projects</h1>
 <div id="project-gallery">
-    <div class="project-card">
-        <img class="project-card-image" src="<?= $request_info['base_url_directory'] ?>/static/images/inspirational_website_preview.gif">
-        <div class="project-card-text">
-            <h2 class="project-card-title">Inspirational Website 2.0</h2>
-            <p class="project-card-description">An inspiration website made with Node.js and React.</p>
+
+    <?php
+        $query = 'SELECT * FROM projects';
+
+        $projects = mysqli_query($db, $query);
+
+        if (!$projects) {
+            exit('Database query failed');
+        }
+    ?>
+
+    <?php while ($project = mysqli_fetch_assoc($projects)) { ?>
+        <div class="project-card">
+            <img class="project-card-image" src="<?= $request_info['base_url_directory'] ?>/static/images/<?= $project['display_image'] ?>">
+            <div class="project-card-text">
+                <h2 class="project-card-title"><?= $project['title'] ?></h2>
+                <p class="project-card-description"><?= $project['description'] ?></p>
+            </div>
         </div>
-    </div>
-    <div class="project-card">
-        <img class="project-card-image" src="<?= $request_info['base_url_directory'] ?>/static/images/tab2xml_preview.gif">
-        <div class="project-card-text">
-            <h2 class="project-card-title">TAB2ML</h2>
-            <p class="project-card-description">Converts text-based music tablature to playable sheet music with Java.</p>
-        </div>
-    </div>
-    <div class="project-card">
-        <img class="project-card-image" src="<?= $request_info['base_url_directory'] ?>/static/images/selection_sort_visualization.gif">
-        <div class="project-card-text">
-            <h2 class="project-card-title">Sorting Algorithm Visualizer</h2>
-            <p class="project-card-description">Visualizes sorting algorithms in real time.</p>
-        </div>
-    </div>
+    <?php
+        }
+
+        mysqli_free_result($projects);
+    ?>
 </div>
