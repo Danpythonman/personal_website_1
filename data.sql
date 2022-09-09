5f2fe201dd8e3ca20239600732640258bbde6011
Daniel Di Giovanni <dannyjdigio@gmail.com>
2022-09-08 22:43:38 -0400

    Added two SQL tables, one for project page sections, another for project images. Also populated the project page sections table with some data.

--- data.sql
+++ data.sql
@@ -6,6 +6,46 @@ INSERT INTO projects (id, title, description, display_image, path_ending) VALUES
     'inspirational-website-2'
 );
 
+INSERT INTO project_page_sections (id, project_id, section_number, section_type, section_content) VALUES (
+    1,
+    1,
+    1,
+    'subheading',
+    'Features of the Backend'
+);
+
+INSERT INTO project_page_sections (id, project_id, section_number, section_type, section_content) VALUES (
+    2,
+    1,
+    2,
+    'paragraph',
+    'The backend is a REST API written in JavaScript (using the Node.js runtime environment). ExpressJS is used for webserver functionality. The backend is organized into models, routes, controllers, and services. The models and database interaction is done through Mongoose, which connects to a MongoDB Atlas database in the cloud. Email functionality is achieved through Sendgrid. JsonWebToken is used for authorization and node-fetch is used to simplify some API calls.'
+);
+
+INSERT INTO project_page_sections (id, project_id, section_number, section_type, section_content) VALUES (
+    3,
+    1,
+    3,
+    'subheading',
+    'Features of the Frontend'
+);
+
+INSERT INTO project_page_sections (id, project_id, section_number, section_type, section_content) VALUES (
+    4,
+    1,
+    4,
+    'paragraph',
+    'The frontend is written in React. The MUI library is used to implement common components like dialogs, accordions, and icon buttons. The frontend uses Axios to simplify API calls to the backend. The frontend is served via GitHub pages.'
+);
+
+INSERT INTO project_page_sections (id, project_id, section_number, section_type, section_content) VALUES (
+    5,
+    1,
+    5,
+    'paragraph',
+    'The quotes come from the database, which is populated by users who submit quotes to the website. The background image comes from NASA''s Astronomy Picture of the Day API.'
+);
+
 INSERT INTO projects (id, title, description, display_image, path_ending) VALUES (
     2,
     'TAB2XML',
--- schema.sql
+++ schema.sql
@@ -5,5 +5,31 @@ CREATE TABLE projects (
     title TEXT,
     description TEXT,
     display_image TEXT,
-    path_ending TEXT
+    path_ending TEXT,
+    PRIMARY KEY (id)
+);
+
+DROP TABLE IF EXISTS project_page_sections;
+
+CREATE TABLE project_page_sections (
+    id INTEGER,
+    project_id INTEGER,
+    section_number INTEGER,
+    section_type TEXT,
+    section_content TEXT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (project_id) REFERENCES projects(id)
+);
+
+DROP TABLE IF EXISTS project_images;
+
+CREATE TABLE project_images (
+    id INTEGER,
+    project_id INTEGER,
+    image_filename TEXT,
+    image_number INTEGER,
+    image_title TEXT,
+    image_description TEXT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (project_id) REFERENCES projects(id)
 );
