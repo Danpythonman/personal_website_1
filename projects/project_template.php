3a32e90234152f84c98d6e0ebd75bcfbcb4e8b08
Daniel Di Giovanni <dannyjdigio@gmail.com>
2022-09-08 22:46:46 -0400

    Used the project page sections table in the database to render individual project pages.

--- projects/project_template.php
+++ projects/project_template.php
@@ -13,3 +13,26 @@
 ?>
 
 <h1 class="page-title"><?= $project['title'] ?></h1>
+
+<?php
+    $query = 'SELECT * FROM project_page_sections WHERE project_id=\'' . $project['id'] . '\'';
+
+    $result = mysqli_query($db, $query);
+
+    if (!$result) {
+        exit('Database query failed');
+    }
+?>
+
+<?php
+    $project_section_array = ['', '', ''];
+    while ($project_page_section = mysqli_fetch_assoc($result)) {
+        if ($project_page_section['section_type'] == 'subheading') {
+            echo '<h2>' . $project_page_section['section_content'] . '</h2>';
+        } else if ($project_page_section['section_type'] == 'paragraph') {
+            echo '<p>' . $project_page_section['section_content'] . '</p>';
+        }
+    }
+
+    mysqli_free_result($result);
+?>
