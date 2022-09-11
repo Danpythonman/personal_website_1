4cda211b5f2f2364026ce743c3f916a127fbdb2e
Daniel Di Giovanni <dannyjdigio@gmail.com>
2022-09-11 18:13:30 -0400

    Added resume pdf and embedded it in the resume page.

--- pages/resume/resume.php
+++ pages/resume/resume.php
@@ -1,1 +1,2 @@
-<h1>Resume</h1>
+<h1 class="page-title">My Resume</h1>
+<iframe id="resume-pdf" src="/<?= getenv('BASE_URL_DIRECTORY') ?>/static/documents/daniel_di_giovanni_resume_2022-09-11.pdf"></iframe>
