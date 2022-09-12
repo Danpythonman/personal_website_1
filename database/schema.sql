73e1ac243c56da0d9904c9c45e5119de730e9327
Daniel Di Giovanni <dannyjdigio@gmail.com>
2022-09-12 17:47:28 -0400

    Added tables to the daabase for resume sections and accomplishments for each resume section. Added data into these two tables.

--- database/schema.sql
+++ database/schema.sql
@@ -32,3 +32,26 @@ CREATE TABLE project_images (
     PRIMARY KEY (id),
     FOREIGN KEY (project_url_endpoint) REFERENCES projects(url_endpoint)
 );
+
+DROP TABLE IF EXISTS resume_sections;
+
+CREATE TABLE resume_sections (
+    id INTEGER NOT NULL AUTO_INCREMENT,
+    section_number INT,
+    title VARCHAR(50),
+    company VARCHAR(50),
+    duration_and_work_type VARCHAR(50),
+    description TEXT,
+    PRIMARY KEY (id)
+);
+
+DROP TABLE IF EXISTS resume_section_accomplishments;
+
+CREATE TABLE resume_section_accomplishments (
+    id INTEGER NOT NULL AUTO_INCREMENT,
+    resume_section_id INT,
+    accomplishment_number INT,
+    content VARCHAR(250),
+    PRIMARY KEY (id),
+    FOREIGN KEY (resume_section_id) REFERENCES resume_sections(id)
+);
