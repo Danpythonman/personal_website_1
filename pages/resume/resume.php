ca8f40a6653b59038fdf429f7a73938ef93322ab
Daniel Di Giovanni <dannyjdigio@gmail.com>
2022-09-12 17:47:54 -0400

    The resume page now uses the database to render the page content.

--- pages/resume/resume.php
+++ pages/resume/resume.php
@@ -1,53 +1,50 @@
 <h1 class="page-title">My Work Experience</h1>
 <p class="page-description">Take a look at my professional experience, as well as my resume in PDF format.</p>
 <h2 class="page-heading">My Professional History</h2>
+<?php
+    $query = 'SELECT * FROM resume_sections ORDER BY section_number DESC';
+
+    $resume_sections = mysqli_query($db, $query);
+
+    if (!$resume_sections) {
+        exit('Database query failed');
+    }
+?>
 <div id="resume-html">
-    <div class="resume-section">
-        <div class="job-header">
-            <div class="job-information">
-                <h3 class="job-title">E-Commerce Advisor</h3>
-                <p class="company">Digital Main Street</p>
-                <p class="duration-and-work-type">June 2022 - Present <i>(Full Time)</i></p>
+    <?php while ($resume_section = mysqli_fetch_assoc($resume_sections)) { ?>
+        <div class="resume-section">
+            <div class="job-header">
+                <div class="job-information">
+                    <h3 class="job-title"><?= $resume_section['title'] ?></h3>
+                    <p class="company"><?= $resume_section['company'] ?></p>
+                    <p class="duration-and-work-type"><?= $resume_section['duration_and_work_type'] ?></p>
+                </div>
+                <p class="job-description"><?= $resume_section['description'] ?></p>
             </div>
-            <p class="job-description">
-                In this position I worked as an e-commerce advisor the government-funded Canada Digital Adoption Program (CDAP).
-                The program essentially helps small businesses who have not made significant progress in bringing their business online.
-                My job was to advise small businesses and give personalized help on digitizing their operations.
-            </p>
+            <h4 class="job-accomplishments-title">What I Accomplished:</h4>
+            <ul class="job-accomplishments">
+                <?php
+                    $query = 'SELECT * FROM resume_section_accomplishments WHERE resume_section_id=\'' . $resume_section['id'] . '\' ORDER BY accomplishment_number';
+
+                    $accomplishments = mysqli_query($db, $query);
+
+                    if (!$accomplishments) {
+                        exit('Database query failed');
+                    }
+
+                    while ($accomplishment = mysqli_fetch_assoc($accomplishments)) {
+                        echo '<li>' . $accomplishment['content'] . '</li>' . PHP_EOL;
+                    }
+
+                    mysqli_free_result($accomplishments);
+                ?>
+            </ul>
         </div>
-        <h4 class="job-accomplishments-title">What I Accomplished:</h4>
-        <ul class="job-accomplishments">
-            <li>Audited clients' websites and gave recommendations for improvements.</li>
-            <li>Assisted five clients in accessing a $2,400 micro-grant.</li>
-            <li>Prepared resources, write-ups, and held meetings about e-commerce, digital marketing, and cybersecurity.</li>
-            <li>Built a total of six websites using Shopify, Square, and Bookmark.</li>
-            <li>Learned SEO best practices, implemented them in demo websites, and instructed clients on them.</li>
-            <li>Created advertising campaigns on Google and Facebook.</li>
-        </ul>
-    </div>
-    <div class="resume-section">
-        <div class="job-header">
-            <div class="job-information">
-                <h3 class="job-title">Full Stack Developer</h3>
-                <p class="company">Dwella Investing</p>
-                <p class="duration-and-work-type">June 2021 - December 2021 <i>(Part Time)</i></p>
-            </div>
-            <p class="job-description">
-                Dwella Investing is a startup based in Toronto, Ontario, Canada whose goal is to use the blockchain to tokenize real estate investments.
-                As a full-stack developer, I worked extensively on the frontend and backend of Dwella's platform.
-                I also had the opportunity to interact with the Ethereum blockchain through working on the frontend.
-            </p>
-        </div>
-        <h4 class="job-accomplishments-title">What I Accomplished:</h4>
-        <ul class="job-accomplishments">
-            <li>Built backend REST API from scratch with lead programmer using Node.js, with the ExpressJS framework.</li>
-            <li>Used MongoDB Atlas for the database and designed schemas with Mongoose.</li>
-            <li>Built frontend dashboard using React and MUI library and connected it to the backend.</li>
-            <li>Implemented JWT-based authorization system with authorization and refresh tokens.</li>
-            <li>Provided technical guidance during meetings with team members in less technical roles.</li>
-            <li>Continually adapted frontend and backend to changing specifications.</li>
-        </ul>
-    </div>
+    <?php
+        }
+
+        mysqli_free_result($resume_sections);
+    ?>
 </div>
 <h2 class="page-heading">My Resume</h2>
 <div class="page-description">
