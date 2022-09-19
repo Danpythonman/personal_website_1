ed5ad516ee7cc34f9ee0bfcf855704d8411ebdb1
Daniel Di Giovanni <dannyjdigio@gmail.com>
2022-09-19 15:02:19 -0400

    Added welcome section to home page (including animated button).

--- pages/home/home.php
+++ pages/home/home.php
@@ -1,1 +1,8 @@
-<h1>Daniel Di Giovanni</h1>
+<div id="welcome-section" class="section">
+    <h1>Welcome!</h1>
+    <p>
+        I'm <b>Daniel Di Giovanni</b>, a programming professional, student, and hobbyist.
+        I'm in my third year of studying software engineering at York University, but I've been programming for about five years in total.
+    </p>
+    <a href="<?= '/' . getenv('BASE_URL_DIRECTORY') . '/about' ?>">Learn more about myself and my education</a>
+</div>
