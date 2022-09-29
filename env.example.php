8be5f75dc0ec0415e79f23ddf25643d489621bfe
Daniel Di Giovanni <dannyjdigio@gmail.com>
2022-09-29 00:51:24 -0400

    Finished contact page. Added form to send email and improved styles of GitHub and LinkedIn icons. Also, I'm using Web3Forms for the contact form. This handles the email being sent to me.

--- env.example.php
+++ env.example.php
@@ -5,4 +5,6 @@
     putenv('DB_USER=database_username');
     putenv('DB_PASSWORD=database_user_password');
     putenv('DB_NAME=database_name');
+
+    putenv('WEB3FORMS_ACCESS_KEY=7f0126ae-14d3-4f4b-b5fd-0cd356a53558');
 ?>
