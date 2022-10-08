b340a100d6ba52ebe7dd409d5abd17b00358808d
Daniel Di Giovanni <dannyjdigio@gmail.com>
2022-10-08 14:21:28 -0400

    If there is an error 404 and the URL ends in a slash, then a prompt is now displayed to try the link without the trailing slash, as well as the hyperlink without the trailing slash.

--- pages/errors/error404.php
+++ pages/errors/error404.php
@@ -1,4 +1,16 @@
+<?php
+    $url = ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on') ? "https://" : "http://") . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+?>
+
 <div id="error-page-header">
     <h1>Error 404 - We couldn't find this page :(</h1>
-    <p class="section paragraph">The page <code>https://<?= $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'] ?></code> could not be found.</p>
+    <p class="section paragraph">The page <code><?= $url ?></code> could not be found.</p>
+    <?php
+        if ($_SERVER['REQUEST_URI'][-1] == '/') {
+            $url_no_slash = substr($url, 0, strlen($url) - 1);
+    ?>
+        <p class="section paragraph">Try without the final slash: <code><a href="<?= $url_no_slash ?>"><?= $url_no_slash ?></a></code></p>
+    <?php
+        }
+    ?>
 </div>
