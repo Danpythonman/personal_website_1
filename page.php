3d38105c73be9c1afc67c8f95fc24ff0cdfb8666
Daniel Di Giovanni <dannyjdigio@gmail.com>
2022-10-10 14:12:57 -0400

    Added favicon

--- page.php
+++ page.php
@@ -3,6 +3,7 @@
     <head>
         <title><?= $page_info['title'] ?></title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="icon" type="image/x-icon" href="/<?= BASE_URL_DIRECTORY ?>static/icons/favicon.ico">
         <link rel="stylesheet" type="text/css" href="/<?= BASE_URL_DIRECTORY ?>static/css/style.css">
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans|Fjalla+One|Didact+Gothic">
     </head>
