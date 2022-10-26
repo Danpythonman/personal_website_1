41a44f216fcc91fa73754996739944619032e9ed
Daniel Di Giovanni <dannyjdigio@gmail.com>
2022-10-26 15:25:18 -0400

    Added canonical link to pages with 'www.' (that are not error pages). This means the non-www version is the canonical version.

--- page.php
+++ page.php
@@ -9,6 +9,34 @@
                 <meta name="description" content="<?= $page_info['meta_description'] ?>">
         <?php
             }
+
+            // Since there are two versions of the website (www and non-www),
+            // I am designating non-www to be the canonical website.
+            // If the URL has www and it not an error, then we add a canonical
+            // link to the non-www version of the URL.
+
+            // Check if page was not an error.
+            // This means either the error field of the page_info associative
+            // array was either not defined, or it is defined and is false.
+            if (
+                (!array_key_exists('error', $page_info))
+                ||
+                (array_key_exists('error', $page_info) && !$page_info['error'])
+            ) {
+                // Check if 'www.' is the first part of the domain
+                if (strpos($_SERVER['HTTP_HOST'], 'www.') === 0) {
+                    // Remove 'www.' from URL
+                    $http_host_without_www = str_replace('www.', '', $_SERVER['HTTP_HOST']);
+
+                    // Re-make the URL without 'www.'
+                    $url_without_www = ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on') ? "https://" : "http://")
+                        . $http_host_without_www
+                        . $_SERVER['REQUEST_URI'];
+        ?>
+                    <link rel="canonical" href="<?= $url_without_www ?>">
+        <?php
+                }
+            }
         ?>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="icon" type="image/x-icon" href="<?= CDN_URL ?>icons/favicon.ico">
