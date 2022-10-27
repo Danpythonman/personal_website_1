0fc51fa0a760b0bcd848550268080c1841e88e28
Daniel Di Giovanni <dannyjdigio@gmail.com>
2022-10-27 13:39:50 -0400

    Added link in the footer to the source code of this project.

--- footer.php
+++ footer.php
@@ -16,6 +16,8 @@
             <li class="footer-link"><a href="<?= '/' . BASE_URL_DIRECTORY . 'resume' ?>">Resume</a></li>
             <li class="footer-link"><a href="<?= '/' . BASE_URL_DIRECTORY . 'about' ?>">About Me</a></li>
             <li class="footer-link"><a href="<?= '/' . BASE_URL_DIRECTORY . 'contact' ?>">Contact Me</a></li>
+            <li class="footer-link-separator"></li>
+            <li class="footer-link"><a href="https://github.com/Danpythonman/personal_website_1" alt="Source code for this website" target="_blank">Source Code <img class="open-link-icon" src="<?= CDN_URL ?>icons/open_link.svg" alt="Open link icon"></a></li>
         </ul>
     </div>
     <p>&copy; <?= date('Y') ?> Daniel Di Giovanni</p>
