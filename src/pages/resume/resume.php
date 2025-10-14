24903a7a3fece22e73f7fb92e08949c469ff30ed
Daniel Di Giovanni <dannyjdigio@gmail.com>
2025-10-13 22:59:44 -0400

    Moved PHP files to src directory.

---
+++ src/pages/resume/resume.php
@@ -0,0 +1,13 @@
+<div class="page-header">
+    <div class="section">
+        <h1 class="page-title">My Work Experience</h1>
+        <p class="paragraph">
+            If the resume is not loading, please try refreshing the page.
+            You can also download my resume PDF by clicking the button below.
+        </p>
+        <a class="link-button button-5" href="<?= CDN_URL ?>documents/daniel_di_giovanni_resume_2025-10-13.pdf" download>Download resume</a>
+    </div>
+</div>
+<div class="section">
+</div>
+<iframe id="resume-pdf" src="<?= CDN_URL ?>documents/daniel_di_giovanni_resume_2025-10-13.pdf"></iframe>
